52f7f1ab7bf515d71a05116f846a8303f958b0b3
cjcj5438 <1002931123@qq.com>
2019-09-21 12:27:24 +0800

    state

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,17 +4,25 @@ interface IProps {
     name: string,
     age?: number
 }
+
+interface  IState {
+    name:string
+}
 // 类型别名
 // type IProps = {
 //     name: string,
 //     age?: number
 // }
-
-export default class Form extends Component<IProps> {
+// 如果没有IProps的话那么就使用 {} 传
+export default class Form extends Component<IProps,IState> {
+    state={
+        name:'陈靖'
+    }
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
         return (
             <div>
                 {this.props.name} - {this.props.age}
+                {this.state.name}
             </div>
         );
     }
